393473989a792b19151153add79b95bd05b702a9
WILLIAMS-creator672 <wisdomwills1@outlook.com>
2025-07-28 17:03:40 +0100

    made changes

--- dashboard.js
+++ dashboard.js
@@ -6,3 +6,11 @@ cart.addEventListener('mouseenter', () => {
 cart.addEventListener('mouseleave', () => {
     cart.classList.remove('animate');
 });
+
+window.addEventListener('DOMContentLoaded', () => {
+    let savedData = localStorage.getItem(JSON.parse('formData'))
+
+    if (savedData){
+        document.getElementById('welcomeMessage').innerHTML = `welcome ${savedData.name} this is you dashboard`
+    }
+})
